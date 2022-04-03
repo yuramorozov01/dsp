ce5e4c26b5eb8d13e887d5604fc774c0747eeedf
yuramorozov01 <yura_morozov01@mail.ru>
2022-04-03 21:53:09 +0300

    feat (frontend): add to history fourier transform results

--- services/frontend/src/app/history-page/history-page.component.ts
+++ services/frontend/src/app/history-page/history-page.component.ts
@@ -4,6 +4,10 @@ import { Observable } from 'rxjs';
 
 import { HarmonicSignalService } from '../shared/services/harmonic-signal/harmonic-signal.service';
 import { IHarmonicSignalList } from '../shared/interfaces/harmonic-signal.interfaces';
+
+import { FourierTransformService } from '../shared/services/fourier-transform/fourier-transform.service';
+import { IFourierTransformList } from '../shared/interfaces/fourier-transform.interfaces';
+
 import { MaterializeService } from '../shared/services/utils/materialize.service';
 
 
@@ -14,18 +18,32 @@ import { MaterializeService } from '../shared/services/utils/materialize.service
 })
 export class HistoryPageComponent implements OnInit {
     harmonicSignals$: Observable<IHarmonicSignalList[]>;
+    fourierTransforms$: Observable<IFourierTransformList[]>;
 
-    constructor(private harmonicSignalService: HarmonicSignalService) {
+    constructor(private harmonicSignalService: HarmonicSignalService,
+                private fourierTransformService: FourierTransformService) {
     }
 
     ngOnInit(): void {
-        this.fetchHarmonicSignals()
+        this.fetchHarmonicSignals();
+        this.fetchFourierTransforms();
     }
 
     private fetchHarmonicSignals() {
         this.harmonicSignals$ = this.harmonicSignalService.fetch();
         this.harmonicSignals$.subscribe(
             (harmonicSignals: IHarmonicSignalList[]) => {
+			},
+			error => {
+				MaterializeService.toast(error.error);
+			}
+        )
+    }
+
+    private fetchFourierTransforms() {
+        this.fourierTransforms$ = this.fourierTransformService.fetch();
+        this.fourierTransforms$.subscribe(
+            (fourierTransforms: IFourierTransformList[]) => {
 			},
 			error => {
 				MaterializeService.toast(error.error);
@@ -41,6 +59,18 @@ export class HistoryPageComponent implements OnInit {
 					response => MaterializeService.toast({'Success': 'Harmonic signal has been deleted successfully'}),
 					error => MaterializeService.toast(error.error),
 					() => this.fetchHarmonicSignals()
+				);
+		}
+	}
+
+    public deleteFourierTransform(id: number) {
+		const decision = window.confirm('Are you sure you want to delete this fourier transform?');
+		if (decision) {
+			this.fourierTransformService.delete(id)
+				.subscribe(
+					response => MaterializeService.toast({'Success': 'Fourier transform has been deleted successfully'}),
+					error => MaterializeService.toast(error.error),
+					() => this.fetchFourierTransforms()
 				);
 		}
 	}
