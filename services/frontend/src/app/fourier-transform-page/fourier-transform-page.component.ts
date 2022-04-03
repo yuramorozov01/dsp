9a363e7898167a307af10f39b7db1382444b7111
yuramorozov01 <yura_morozov01@mail.ru>
2022-04-03 16:26:58 +0300

    feat (frontend): add service with interfaces for fourier transform

---
+++ services/frontend/src/app/fourier-transform-page/fourier-transform-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-fourier-transform-page',
+    templateUrl: './fourier-transform-page.component.html',
+    styleUrls: ['./fourier-transform-page.component.css']
+})
+export class FourierTransformPageComponent implements OnInit {
+    // ^\d+(?:,\d+)*\Z
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
---
+++ services/frontend/src/app/shared/interfaces/fourier-transform.interfaces.ts
@@ -0,0 +1,23 @@
+import { IUserMe } from './auth.interfaces';
+
+export interface IFourierTransformList {
+    id: number;
+    author: IUserMe;
+    amplitudes: string;
+    frequencies: string;
+    task_id: string;
+}
+
+export interface IFourierTransform {
+    id: number;
+    author: IUserMe;
+    amplitudes: string;
+    frequencies: string;
+    task_id: string;
+}
+
+export interface IFourierTransformResult {
+    time: number[];
+    result_values: number[];
+    harmonics_values: number[][];
+}
